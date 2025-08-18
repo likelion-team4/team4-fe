9465e0973d29f9a9bd9e1e20d798d3274160a735
parkjihyoun <jihyoun2002@knu.ac.kr>
2025-08-18 22:48:11 +0900

    badgepage 수정

--- src/pages/BadgesPage.tsx
+++ src/pages/BadgesPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BadgeCard from "../components/badge/BadgeCard";
 import {
   ShoppingBag,
@@ -25,8 +25,15 @@ const badges = [
 ];
 
 const BadgesPage: React.FC = () => {
+  const [toast, setToast] = useState(false);
+
+  const handleBadgeClick = () => {
+    setToast(true);
+    setTimeout(() => setToast(false), 2000); // 2초 후 사라짐
+  };
+
   return (
-    <div className="mx-auto w-full max-w-[720px] px-12 pt-6 pb-10">
+    <div className="relative mx-auto w-full max-w-[720px] px-12 pt-6 pb-10">
       <h2 className="mb-4 mt-4 text-[18px] font-semibold text-black">나의 뱃지</h2>
 
       <div className="grid grid-cols-3 gap-x-4 gap-y-6">
@@ -37,9 +44,21 @@ const BadgesPage: React.FC = () => {
             icon={b.icon}
             locked={!!b.locked}
             bgClassName={b.bgClassName}
+            onClick={handleBadgeClick} // 클릭 이벤트 연결
           />
         ))}
       </div>
+
+      {/* 토스트 */}
+      {toast && (
+        <div
+          className="fixed top-30 left-1/2 -translate-x-1/2 
+                     rounded-lg bg-[rgba(0,120,248,0.8)] text-white text-sm font-bold px-4 py-2 shadow-lg
+                     animate-fade-in-up"
+        >
+          뱃지를 획득하였습니다!
+        </div>
+      )}
     </div>
   );
 };
